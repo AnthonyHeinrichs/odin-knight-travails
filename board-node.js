5d85d71a2c655cc2bbc07ceeae25877b6bc7869b
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-16 07:09:44 +0100

    Importing board node and confirmed nodes are being created

---
+++ board-node.js
@@ -0,0 +1,5 @@
+const Node = (row, col, distanceFromStart) => {
+  return { row, col, distanceFromStart }
+}
+
+export default Node
---
+++ shortest-path.js
@@ -0,0 +1,9 @@
+import Node from './board-node.js'
+
+const knightsShortestPath = (x,y) => {
+  const queue = []
+  const startNode = Node(0, 0, 0)
+  console.log(startNode)
+}
+
+knightsShortestPath()
